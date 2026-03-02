ba69af3c277da397ab2dd85a58b31092ab1542bb
pseudodev3 <walebadejoko@gmail.com>
2026-03-02 12:01:03 +0100

    Increase particle count to 800 with object pooling

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ function initAshCanvas() {
     const ctx = canvas.getContext('2d');
 
     let width, height;
-    const count = 300;
+    const count = 800;
     const pool = new Float32Array(count * 3);
 
     function resize() {
